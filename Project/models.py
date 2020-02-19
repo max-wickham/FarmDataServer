17f7286cbe48a6fcdb25448db2b239552863d565
max-wickham <maximus.wickham19@imperial.ac.uk>
2020-02-19 14:37:28 +0000

    Adding Models to database and app routes Models added: 	Thread 	Comment 	WeatherReport 	LiveStockReport 	CropReport 	FarmInfoLiveStock 	FarmInforCrop Routes Added: 	get_username_avaialable 	get_email_available 	get_thread 	get_comments

--- Project/models.py
+++ Project/models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 from __main__ import db
+from enum import Enum
 
 
 
@@ -11,7 +12,18 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(64))    
+    email = db.Column(db.String(64), index = True)
+    password_hash = db.Column(db.String(64))
+    verified = db.Column(db.Boolean)
+    #threads written by the user links to threads table
+    threads = db.relationship('Thread', backref='author', lazy='dynamic')
+    #reports for the user
+    weather_reports = db.relationship('WeatherReports', backref='user', lazy='dynamic')
+    livestock_reports = db.relationship('LiveStockReport', backref='user', lazy='dynamic')
+    crop_reports = db.relationship('CropReport', backref='user', lazy='dynamic')
+    #farminfo profile information for the user
+    farminfo_livestocks = db.relationship('FarmInfoLiveStock', backref='user', lazy='dynamic')
+    farminfo_lcrops = db.relationship('FarmInfoCrop', backref='user', lazy='dynamic')
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
@@ -19,4 +31,82 @@ class User(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
-     
+    #def generate_report(self):
+        ##TODO
+
+    #def generate_threads(self):
+
+
+
+class Thread(db.Model):
+    __tablename__ = 'threads'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), index=True)
+    description = db.Column(db.String(1000), index=True)
+    image_path = db.Column(db.String(32), index=True)
+    #timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    posts = db.relationship('Comment', backref='thread', lazy='dynamic')
+
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(1000), index=True)
+    image_path = db.Column(db.String(32), index=True)
+    #timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    thread_id = db.Column(db.Integer, db.ForeignKey('threads.id'))
+
+
+
+class WeatherReport(db.Model):
+    __tablename__ = 'weather_reports'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(64), index=True)
+    #timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    warning_level = db.Column(db.Integer, index = True) #Warning level from 0 to 10
+    description = db.Column(db.String(240), index=True)
+    
+
+
+class LiveStockReport(db.Model):
+    __tablename__ = 'livestock_reports'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(64), index=True)
+    #timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    warning_level = db.Column(db.Integer, index = True) #Warning level from 0 to 10
+    description = db.Column(db.String(240), index=True)
+
+class CropReport(db.Model):
+    __tablename__ = 'crop_reports'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(64), index=True)
+    #timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    warning_level = db.Column(db.Integer, index = True) #Warning level from 0 to 10
+    description = db.Column(db.String(240), index=True)
+
+
+class FarmInfoLiveStock(db.Model):
+    __tablename__ = 'farminfo_livestocks'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    livestock = db.Column(db.String(32), index=True)
+    size = db.Column(db.Integer, index = True) #size in km^2
+    latitude = db.Column(db.Integer, index = True)
+    longitude = db.Column(db.Integer, index = True)
+
+class FarmInfoCrop(db.Model): 
+    __tablename__ = 'farminfo_crops'   
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    crop = db.Column(db.String(32), index=True)
+    size = db.Column(db.Integer, index = True) #size in km^2
+    latitude = db.Column(db.Integer, index = True)
+    longitude = db.Column(db.Integer, index = True)
--- Project/routes.py
+++ Project/routes.py
@@ -5,39 +5,90 @@ from __main__ import auth
 from models import User
 from __main__ import db
 
+@app.route('/', methods=['GET'])
+def root():
+    return "FarmData"
+
+
 @app.route('/register', methods=['POST'])
 def register():
     username = request.json.get('username')
+    email = reques.json.get('username')
     password = request.json.get('password')
-    if username is None or password is None:
+    if username is None or password is None or email is None:
         return 'missing username or password'    # missing arguments
     if User.query.filter_by(username=username).first() is not None:
-        return 'existing user'   # existing user
-    user = User(username=username)
+        return 'existing_user'   # existing user
+    if User.query.filter_by(email=email).first() is not None:
+        return 'existing_email'   # existing user
+    user = User(username=username,email=email,verified=True)
     user.hash_password(password)
     db.session.add(user)
     db.session.commit()
-    return 'added user'
+    return 'added_user'
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
-        return 'missing username or password'    # missing arguments
+        return 'missing_username_or_password'    # missing arguments
     user = User.query.filter_by(username=username).first()
     if user is not None:
         if user.verify_password(password):
             return 'logged in'
-        return 'no user found'
-    return 'error'
+        return 'password_incorrect'
+    return 'no_user_found'
 
-@app.route('/', methods=['GET'])
-def root():
-    return "FarmData"
+
 
 @app.route('/profile')
 @auth.login_required
 def get_resource():
     return 'username:' + auth.username()
 
+
+@app.route('/usernameavailable', methods=['POST'])
+def get_username_avaialbe():
+    username = request.json.get('username')
+    if User.query.filter_by(username=username) is not None:
+        return 'available'
+    return 'unavailable'
+
+
+@app.route('/emailavaialble', methods=['POST'])
+def ge_email_available():
+    email = request.json.get('email')
+    if User.query.filter_by(email=email).first() is not None:
+        return 'unavailable'
+    return 'available'
+
+
+
+@app.route('/getthread', methods=['POST'])
+def get_thread():
+    id = request.json.get('id')
+    thread = Thread.query.filter_by(id=id).first()
+    if thread is not None:
+        user = (User.query.filter_by(id=thread.user_id).first()).username
+        if user is None:
+            user = ""
+        
+        response = {
+            'title':thread.title,
+            'description':thread.description,
+            'image_path':thread.image_path,
+            'user':user,
+        }
+        return response
+
+@app.route('/getcomments', methods=['POST'])
+def get_comments():
+    id = request.json.get('id')
+    comments = Comment.query.filter_by(thread_id=id)
+    for comment in comments:
+        user = (User.query.filter_by(id=comment.user_id).first()).username
+        if user is None:
+            user = ""
+        response[str(comment.id)] = [comment.description,comment.imagepath,user]
+    return response   
